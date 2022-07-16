d6abcc8efd8c371c50d4dc2380ec1a012d9ef896
diptoprodipto <diptoprodipto@gmail.com>
2022-07-16 12:37:18 +0600

    routes restored

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/attendance', [AttendanceController::class, 'index']);
+Route::post('/attendance/save', [AttendanceController::class, 'store']);
+Route::post('/attendance/saveall', [AttendanceController::class, 'store_all']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
